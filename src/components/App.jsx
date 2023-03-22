ae5be958b6920b67e9591147eed07f6a38594f77
Siracenco Serghei <siracencoserghei@gmail.com>
2023-03-22 18:26:52 +0100

    add some task exemples

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,17 @@ import { EventBoard } from "./EventBoard/EventBoard";
 import { PageTitle } from "./PageTitle/PageTitle";
 import upcomingEvent from "../upcoming-events.json";
 
+
+// export const App = () => {
+//   return (<div>
+//     <PageTitle text="24th Core Worlds Coalition Conference" />
+//     <EventBoard events={upcomingEvent} />
+//   </div>);
+// };
+
 export const App = () => {
-  return (<div>
+  return (<>
     <PageTitle text="24th Core Worlds Coalition Conference" />
     <EventBoard events={upcomingEvent} />
-  </div>);
+  </>);
 };
--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,14 +1,51 @@
+import PropTypes from 'prop-types'; 
+import { FaMapMarkerAlt, FaUserAlt, FaCalendarAlt, FaClock } from 'react-icons/fa';
+import { formatEventStart } from 'components/utils/formatEventStart';
+import { formatEventDuration } from 'components/utils/formatEventDuration';
+import css from './Event.module.css';
+
+// export const Event = ({name, location, speaker, type, start, end}) => { 
+//     return (
+//         <div className={css.event}>
+//             <h2 className={css.title}>{name}</h2>
+//             <p className={css.info}><i className="icon"></i>location}</p>
+//             <p className={css.info}><i className="icon"></i>{speaker}</p>
+//             <p className={css.info}><i className="icon"></i>{start}</p>
+//             <p className={css.info}><i className="icon"></i>Duration</p>
+//             {/* <span className="chip free|paid|vip">Event type</span> */}
+//         </div>
+//     )
+// };
 
 
-export const Event = ({name, location, speaker, type, start, end}) => { 
+// export const Event = ({name, location, speaker, type, start, end}) => { 
+//     return (
+//         <div className={css.event}>
+//             <h2 className={css.title}>{name}</h2>
+//             <p className={css.info}><FaMapMarkerAlt className={css.icon} />location}</p>
+//             <p className={css.info}><FaUserAlt className={css.icon} />{speaker}</p>
+//             <p className={css.info}><FaCalendarAlt className={css.icon} />{start}</p>
+//             <p className={css.info}><FaClock className={css.icon} />Duration</p>
+//             {/* <span className="chip free|paid|vip">Event type</span> */}
+//         </div>
+//     )
+// };
+ 
+
+export const Event = ({ name, location, speaker, type, start, end }) => {
+    const formattedStartDate = formatEventStart(start);
+    const duration = formatEventDuration(start, end);
     return (
-        <div className="event">
-            <h2 className="title">name</h2>
-            <p className="info"><i className="icon"></i>Location</p>
-            <p className="info"><i className="icon"></i>Speaker</p>
-            <p className="info"><i className="icon"></i>Start Date</p>
-            <p className="info"><i className="icon"></i>Duration</p>
-            <span className="chip free|paid|vip">Event type</span>
+        <div className={css.event}>
+            <h2 className={css.title}>{name}</h2>
+            <p className={css.info}><FaMapMarkerAlt className={css.icon} />{location}</p>
+            <p className={css.info}><FaUserAlt className={css.icon} />{speaker}</p>
+            <p className={css.info}><FaCalendarAlt className={css.icon} />{formattedStartDate}</p>
+            <p className={css.info}><FaClock className={css.icon} />{duration}</p>
+            {/* <span className="chip free|paid|vip">Event type</span> */}
         </div>
     )
 };
+
+
+Event.propTypes = { name: PropTypes.string.isRequired, location: PropTypes.string.isRequired, speaker: PropTypes.string.isRequired, type: PropTypes.string.isRequired, start: PropTypes.string.isRequired, end: PropTypes.string.isRequired }
