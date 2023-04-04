da0b4edc0c9f1ebc3f0a660cf19f423204e9bb25
Siracenco Serghei <siracencoserghei@gmail.com>
2023-04-04 18:52:23 +0200

    test1 catalog

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,8 @@
 
-import upcomingEvent from "../upcoming-events.json";
-import { EventBoard } from "./EventBoard/EventBoard";
-import { PageTitle } from "./PageTitle/PageTitle";
-import {ButtonClick} from "./ButtonClick/ButtonClick"
-import { MyComponent } from "./MyComponent/MyComponent";
 
-// export const App = () => {
-//   return (<div>
-//     <PageTitle text="24th Core Worlds Coalition Conference" />
-//     <EventBoard events={upcomingEvent} />
-//   </div>);
-// };
 
 export const App = () => {
   return (<>
-    <MyComponent />
-    <PageTitle text="24th Core Worlds Coalition Conference" />
-    <EventBoard events={upcomingEvent} />
-    <ButtonClick />
-    <MyComponent />
+    
   </>);
 };
--- src/components/PageTitle/PageTitle.jsx
+++
@@ -1,9 +0,0 @@
-import css from './PageTitele.module.css';
-import PropTypes from 'prop-types';
-
-export const PageTitle = ({text}) => { 
-    return <h1 className={css.title}>{text}</h1>
-};
-PageTitle.propTypes = {
-    text: PropTypes.string.isRequired
-};
