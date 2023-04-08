3eb10b980acb36dd8d0b758976af9d4018c81eb4
Siracenco Serghei <siracencoserghei@gmail.com>
2023-04-08 09:54:28 +0200

    Todo list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
 
-import React from "react";
+import React, { Component } from "react";
 import Counter from "./Counter";
 import Dropdown from "./Dropdown";
 import ColorPicker from "./ColorPicker";
+import TodoList from './TodoList';
+// import todos from './todos.json';
+import initialTodos from '../todos.json';
+
 import './App.css';
 
 const colorPickerOptions = [
@@ -14,16 +18,61 @@ const colorPickerOptions = [
   { label: 'indigo', color: '#3F51B5'},
 ];
 
-export const App = () => {
-  return (<div className="app__container">
+class App extends Component {
+  state = {
+    // todos,
+    todos: initialTodos,
+  };
+
+  deleteTodo = (todoId) => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId)
+    }))
+  }
+
+  render() {
+    const {todos} = this.state;
+    const totalTodoCount = todos.length;
+
+    const completedTodoCount = todos.reduce((acc, todo) => (
+      todo.completed ? acc +1 : acc ),
+       0,);
+    return ( 
+<div className="app__container">
  
-   <h1 className="app__title">component state</h1>
-   <div className="app__content">
-     <Dropdown className="app__dropdown"/>
-      <Counter className="app-counter" initialValue={10}
-      />
-      <ColorPicker className="app-color-picker" options={colorPickerOptions} />
-   </div>
-  </div>);
+ <h1 className="app__title">component state</h1>
+ <div className="app__content">
+   <Dropdown className="app__dropdown"/>
+    <Counter className="app-counter" initialValue={10}
+    />
+    <ColorPicker className="app-color-picker" options={colorPickerOptions} />
+
+<div>
+  <p>total TODO: {totalTodoCount}</p>
+  <p>number of completed TODO: {completedTodoCount}</p>
+</div>
+
+    <TodoList todos={todos} onDeleteTodo={this.deleteTodo}/>
+ </div>
+</div>
+    );
+  }
+}
+
+export default App;
+
+// ==========================================================
+
+// export const App = () => {
+//   return (<div className="app__container">
+ 
+//    <h1 className="app__title">component state</h1>
+//    <div className="app__content">
+//      <Dropdown className="app__dropdown"/>
+//       <Counter className="app-counter" initialValue={10}
+//       />
+//       <ColorPicker className="app-color-picker" options={colorPickerOptions} />
+//    </div>
+//   </div>);
   
-};
+// };
---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TodoList.css';
+
+// const TodoList = ({todos}) => <ul>{todos.map(todo =>
+const TodoList = ({ todos, onDeleteTodo }) => (
+  <ul className='todo__list'>
+    {todos.map(({ id, text }) => (
+      <li key={id} className='todo__item'>
+        <p>{text}</p>
+        <button type='button' onClick={()=>onDeleteTodo(id)}>delete</button>
+      </li>
+    ))}
+  </ul>
+);
+
+export default TodoList;
