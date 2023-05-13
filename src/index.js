79a9f58feb1ad72ff019a4415250840f45308ee8
Siracenco Serghei <siracencoserghei@gmail.com>
2023-05-13 22:17:33 +0200

    change to React(18)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import { ThemeProvider } from '@emotion/react';
 import './index.css';
 import { App } from 'components/App/App';
 import { theme } from './constants';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
